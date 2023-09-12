5792c509eca423b97816e6fa7f703bdf43ba2844
Samshad Ali <sam606166@gmail.com>
2023-09-12 16:57:37 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const btn = document.querySelector('.bar');
+const phonnav = document.querySelector('.phone-nav')
+btn.addEventListener('click',()=>{
+    phonnav.classList.toggle('phone-nav-come')
+})
